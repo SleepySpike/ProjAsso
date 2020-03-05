9a4ce4e9231dba48715411662d60ae2c279919bd
hugo Rosnoblet <hugo.rosnoblet@yahoo.fr>
2020-03-05 16:53:18 +0100

    Ajoutez des fichiers projet.

---
+++ ProjAsso/Controllers/AdherentsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjAsso;
+
+namespace ProjAsso.Controllers
+{
+    public class AdherentsController : Controller
+    {
+        private ProjAssoEntities db = new ProjAssoEntities();
+
+        //GET
+        public ActionResult Connexion()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Connexion([Bind(Include = "Login,Password")] Adherent adherent)
+        {
+            if (ModelState.IsValid)
+            {
+                var adherents = db.Adherents.FirstOrDefault(a => a.Login == adherent.Login && a.Password == adherent.Password);
+
+                if (adherents != null)
+                {
+                    Session["Adherent"] = adherent;
+                    return RedirectToAction("Index", "Sorties");
+                }
+                else
+                {
+                    ViewBag.Erreur = "Erreur de connexion";
+                }
+            }
+            return View();
+        }
+
+        // GET: Adherents
+        public ActionResult Index()
+        {
+            var adherents = db.Adherents.Include(a => a.Association);
+            return View(adherents.ToList());
+        }
+
+        // GET: Adherents/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Adherent adherent = db.Adherents.Find(id);
+            if (adherent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(adherent);
+        }
+
+        // GET: Adherents/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdAssociation = new SelectList(db.Associations, "IdAssociation", "Nom");
+            return View();
+        }
+
+        // POST: Adherents/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdAdherent,IdAssociation,Matricule,Nom,Prenom,Email,Telephone,Cotisation,Login,Password,Responsable,Solde")] Adherent adherent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Adherents.Add(adherent);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdAssociation = new SelectList(db.Associations, "IdAssociation", "Nom", adherent.IdAssociation);
+            return View(adherent);
+        }
+
+        // GET: Adherents/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Adherent adherent = db.Adherents.Find(id);
+            if (adherent == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdAssociation = new SelectList(db.Associations, "IdAssociation", "Nom", adherent.IdAssociation);
+            return View(adherent);
+        }
+
+        // POST: Adherents/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdAdherent,IdAssociation,Matricule,Nom,Prenom,Email,Telephone,Cotisation,Login,Password,Responsable,Solde")] Adherent adherent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(adherent).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdAssociation = new SelectList(db.Associations, "IdAssociation", "Nom", adherent.IdAssociation);
+            return View(adherent);
+        }
+
+        // GET: Adherents/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Adherent adherent = db.Adherents.Find(id);
+            if (adherent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(adherent);
+        }
+
+        // POST: Adherents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Adherent adherent = db.Adherents.Find(id);
+            db.Adherents.Remove(adherent);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProjAsso/Controllers/SortiesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjAsso;
+
+namespace ProjAsso.Controllers
+{
+    public class SortiesController : Controller
+    {
+        private ProjAssoEntities db = new ProjAssoEntities();
+
+        // GET: Sorties
+        public ActionResult Index()
+        {
+            var sorties = db.Sorties.Include(s => s.Association);
+            return View(sorties.ToList());
+        }
+
+        // GET: Sorties/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sortie sortie = db.Sorties.Find(id);
+            if (sortie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sortie);
+        }
+
+        // GET: Sorties/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdAssociation = new SelectList(db.Associations, "IdAssociation", "Nom");
+            return View();
+        }
+
+        // POST: Sorties/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdSortie,IdAssociation,Nom,Prix,Description,Date,CapaciteMinimum,CapaciteMaximum")] Sortie sortie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sorties.Add(sortie);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdAssociation = new SelectList(db.Associations, "IdAssociation", "Nom", sortie.IdAssociation);
+            return View(sortie);
+        }
+
+        // GET: Sorties/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sortie sortie = db.Sorties.Find(id);
+            if (sortie == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdAssociation = new SelectList(db.Associations, "IdAssociation", "Nom", sortie.IdAssociation);
+            return View(sortie);
+        }
+
+        // POST: Sorties/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdSortie,IdAssociation,Nom,Prix,Description,Date,CapaciteMinimum,CapaciteMaximum")] Sortie sortie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sortie).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdAssociation = new SelectList(db.Associations, "IdAssociation", "Nom", sortie.IdAssociation);
+            return View(sortie);
+        }
+
+        // GET: Sorties/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sortie sortie = db.Sorties.Find(id);
+            if (sortie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sortie);
+        }
+
+        // POST: Sorties/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sortie sortie = db.Sorties.Find(id);
+            db.Sorties.Remove(sortie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
